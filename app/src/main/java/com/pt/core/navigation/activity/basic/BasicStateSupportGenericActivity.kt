10e8f8a4f93f4e123d7b4ebcb06a4858207b19a5
Phuong Tran <phuong.tran@zalora.com>
2021-01-22 10:12:19 +0700

    Refactor code

--- app/src/main/java/com/pt/core/navigation/activity/basic/BasicStateSupportGenericActivity.kt
+++ app/src/main/java/com/pt/core/navigation/activity/basic/BasicStateSupportGenericActivity.kt
@@ -2,8 +2,8 @@ package com.pt.core.navigation.activity.basic
 
 import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
-import com.pt.core.state.manual.statebase.NavigationSimpleHandler
+import com.pt.core.state.manual.generic.NavigationSimpleGenericHandler
 
 abstract class BasicStateSupportGenericActivity<State : Parcelable, Event : Parcelable, SideEffect : Parcelable> :
-    AppCompatActivity(), NavigationSimpleHandler<State, Event, SideEffect> {
+    AppCompatActivity(), NavigationSimpleGenericHandler<State, Event, SideEffect> {
 }
--- app/src/main/java/com/pt/core/navigation/fragment/generic/basic/BasicStateSupportGenericFragment.kt
+++ app/src/main/java/com/pt/core/navigation/fragment/generic/basic/BasicStateSupportGenericFragment.kt
@@ -2,10 +2,10 @@ package com.pt.core.navigation.fragment.generic.basic
 
 import android.os.Parcelable
 import androidx.fragment.app.Fragment
-import com.pt.core.state.manual.statebase.NavigationSimpleHandler
+import com.pt.core.state.manual.generic.NavigationSimpleGenericHandler
 
 abstract class BasicStateSupportGenericFragment<State : Parcelable, Event : Parcelable, SideEffect : Parcelable> :
     Fragment(),
-    NavigationSimpleHandler<State, Event, SideEffect> {
+    NavigationSimpleGenericHandler<State, Event, SideEffect> {
     protected abstract val TAG: String
 }
