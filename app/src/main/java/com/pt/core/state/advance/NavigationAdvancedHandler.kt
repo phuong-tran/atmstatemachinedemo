327daf92b4a8b02cdcaeeb98100c90d601daa628
Phuong Tran <phuong.tran@zalora.com>
2021-01-22 09:37:46 +0700

    Refactor code

--- app/src/main/java/com/pt/core/state/advance/NavigationAdvancedHandler.kt
+++ app/src/main/java/com/pt/core/state/advance/NavigationAdvancedHandler.kt
@@ -4,6 +4,12 @@ import android.os.Parcelable
 import com.pt.core.state.common.SavedStateGenericHandler
 import com.pt.core.state.common.StateTransitionGenericProvider
 
+/*
+interface NavigationAdvancedHandler<State : Parcelable, Event : Parcelable, SideEffect : Parcelable> :
+    StateTransitionGenericProvider<State, Event, SideEffect>,
+    SavedStateGenericHandler<State, Event, SideEffect>*/
+
+
 interface NavigationAdvancedHandler<State : Parcelable, Event : Parcelable, SideEffect : Parcelable> :
     StateTransitionGenericProvider<State, Event, SideEffect>,
     SavedStateGenericHandler<State, Event, SideEffect>
