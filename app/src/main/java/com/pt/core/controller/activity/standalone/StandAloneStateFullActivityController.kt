3e828b10c2af9afe5f5b0d617b96f621a1276c66
Phuong Tran <phuong.tran@zalora.com>
2021-01-29 09:49:10 +0700

    refactor code

--- app/src/main/java/com/pt/core/controller/activity/standalone/StandAloneStateFullActivityController.kt
+++ app/src/main/java/com/pt/core/controller/activity/standalone/StandAloneStateFullActivityController.kt
@@ -3,7 +3,7 @@ package com.pt.core.controller.activity.standalone
 import com.pt.core.state.manager.StateFullContextProviderTemplate
 import com.pt.core.state.provider.template.StateFullContextProvider
 
-abstract class StandAloneStateFullActivityController : StandAloneStateReadWriteActivityController() {
+abstract class StandAloneStateFullActivityController : StandAloneStateRecoverableActivityController() {
     final override val stateContext: StateFullContextProvider by lazy {
         StateFullContextProviderTemplate.create(this, this, this)
     }
